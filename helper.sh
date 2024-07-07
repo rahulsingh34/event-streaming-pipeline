594343af6270f680fe6036d9f9a593b7724c2ab1
Rahul Singh <singh434rahul@gmail.com>
2024-07-07 15:09:00 -0400

    bash helpers

---
+++ helper.sh
@@ -0,0 +1,17 @@
+# Access container
+docker exec -it <kafka_container_id> /bin/bash
+
+# Create Topic
+/usr/bin/kafka-topics --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+
+# List contents with kafka in name
+ls /usr/bin | grep kafka
+
+# List topics 
+/usr/bin/kafka-topics --list --bootstrap-server localhost:9092
+
+# Produce message
+/usr/bin/kafka-console-producer --topic test-topic --bootstrap-server localhost:9092
+
+# Consume message
+/usr/bin/kafka-console-consumer --topic test-topic --bootstrap-server localhost:9092
