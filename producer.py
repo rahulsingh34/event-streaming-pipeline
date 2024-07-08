7e519f11846e8ec19c1a71d1b9aa0fe39c417b13
Rahul Singh <singh434rahul@gmail.com>
2024-07-08 16:54:27 -0400

    code to simulate a data producer

---
+++ producer.py
@@ -0,0 +1,24 @@
+from kafka import KafkaProducer
+import json
+import time
+import random
+
+# Function that will generate random data to simulate user activity
+def data_generator():
+    events = ['play', 'pause', 'stop', 'next', 'previous', 'volume_up', 'volume_down', 'like', 'dislike']
+    return {
+        'user_id': random.randint(1000, 9999),
+        'activity': random.choice(events),
+        'timestamp': int(time.time())
+    }
+
+# Create producer
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092', 
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# Send random data every second
+while True:
+    producer.send('listening-activity', data_generator())
+    time.sleep(1)
