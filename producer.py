cc0c2b5a9856644ba952febc194cf6c83df4c0f6
Rahul Singh <singh434rahul@gmail.com>
2025-02-24 19:31:15 -0500

    added ip addresses to producer

--- producer.py
+++ producer.py
@@ -6,10 +6,23 @@ import random
 # Function that will generate random data to simulate user activity
 def data_generator():
     events = ['play', 'pause', 'stop', 'next', 'previous', 'volume_up', 'volume_down', 'like', 'dislike']
+    ip_addresses = [
+        "80.186.25.56",
+        "234.169.177.211",
+        "4.155.1.125",
+        "185.112.234.131",
+        "191.123.192.133",
+        "236.144.89.213",
+        "230.157.219.106",
+        "237.243.71.207",
+        "26.140.16.35",
+        "220.161.178.87"
+    ]
     return {
         'user_id': random.randint(1000, 9999),
         'activity': random.choice(events),
-        'timestamp': int(time.time())
+        'timestamp': int(time.time()),
+        'ip_address': random.choice(ip_addresses)
     }
 
 # Create producer
