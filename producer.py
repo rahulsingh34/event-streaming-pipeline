67ef3785d748aefea0132ebbb2e2b833354ed8b5
Rahul Singh <singh434rahul@gmail.com>
2025-03-04 19:24:42 -0500

    added producer to container

--- producer.py
+++ producer.py
@@ -27,7 +27,7 @@ def data_generator():
 
 # Create producer
 producer = KafkaProducer(
-    bootstrap_servers = 'localhost:9092', 
+    bootstrap_servers='kafka:9092',
     value_serializer = lambda v: json.dumps(v).encode('utf-8')
 )
 
